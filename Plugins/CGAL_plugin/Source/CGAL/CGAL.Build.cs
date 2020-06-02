e5e6eaa326d041189ddf670fc53905630296a5ef
LuoYu <hl1282456555@outlook.com>
2020-06-02 13:22:30 +0800

    CGAL plugin fles

---
+++ Plugins/CGAL_plugin/Source/CGAL/CGAL.Build.cs
@@ -0,0 +1,108 @@
+//MIT License
+
+//Copyright(c) 2018 annsar
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class CGAL : ModuleRules
+{
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
+
+	public CGAL(ReadOnlyTargetRules Target) : base(Target)
+	{
+        PublicDefinitions.Add("WIN32");
+        PublicDefinitions.Add("_WINDOWS");
+        PublicDefinitions.Add("_CRT_SECURE_NO_DEPRECATE");
+        PublicDefinitions.Add("_SCL_SECURE_NO_DEPRECATE");
+        PublicDefinitions.Add("_CRT_SECURE_NO_WARNINGS");
+        PublicDefinitions.Add("_SCL_SECURE_NO_WARNINGS");
+        PublicDefinitions.Add("CGAL_USE_MPFR");
+        PublicDefinitions.Add("CGAL_USE_GMP");
+
+        // Startard Module Dependencies
+        PublicDependencyModuleNames.AddRange(new string[] { "Core" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Slate", "SlateCore", "Projects" });
+
+        // Start CGAL linking here!
+        bool isLibrarySupported = false;
+
+        // Create CGAL Path
+        string CGALPath = Path.Combine(ThirdPartyPath, "CGAL");
+
+        // Get Library Path
+        string LibPath = "";
+        bool isdebug = Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT;
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            LibPath = Path.Combine(CGALPath, "libraries", "Win64");
+            isLibrarySupported = true;
+        }
+        else
+        {
+            string Err = string.Format("{0} dedicated server is made to depend on {1}. We want to avoid this, please correct module dependencies.", Target.Platform.ToString(), this.ToString()); System.Console.WriteLine(Err);
+        }
+
+        if (isLibrarySupported)
+        {
+            //Add Include path
+            PublicIncludePaths.AddRange(new string[] { Path.Combine(CGALPath, "includes") });
+            PublicIncludePaths.AddRange(new string[] { Path.Combine(CGALPath, "includes","GMP") });//Dependencie
+
+            // Add Dependencies
+            if (!isdebug)
+            {
+                //Add Static Libraries
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libgmp-10.lib"));
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libmpfr-4.lib"));
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libCGAL-vc140-mt-4.9.lib"));
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libCGAL_Core-vc140-mt-4.9.lib"));
+            }
+            else
+            {
+                //Add Static Libraries (Debug Version)
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libgmp-10.lib"));
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libmpfr-4.lib"));
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libCGAL-vc140-mt-gd-4.9.lib"));
+                PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libCGAL_Core-vc140-mt-gd-4.9.lib"));
+            }
+
+            PublicDelayLoadDLLs.Add(Path.Combine(LibPath, "libgmp-10.dll"));
+            PublicDelayLoadDLLs.Add(Path.Combine(LibPath, "libmpfr-4.dll"));
+            //RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibPath, "libgmp-10.dll")));
+            //RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibPath, "libmpfr-4.dll")));
+            RuntimeDependencies.Add(Path.Combine(LibPath, "libgmp-10.dll"));
+            RuntimeDependencies.Add(Path.Combine(LibPath, "libmpfr-4.dll"));
+        }
+
+        PublicDefinitions.Add(string.Format("WITH_CGAL_BINDING={0}", isLibrarySupported ? 1 : 0));
+        bEnableExceptions = true;
+
+        bEnableUndefinedIdentifierWarnings = false;
+    }
+}
---
+++ Plugins/CGAL_plugin/Source/CGAL/Private/CGAL.cpp
@@ -0,0 +1,80 @@
+﻿//MIT License
+
+//Copyright(c) 2018 annsar
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+#include "CGAL.h"
+#include "CGALPrivatePCH.h"
+#include "Core.h"
+#include "Modules/ModuleManager.h"
+#include "IPluginManager.h"
+
+#define LOCTEXT_NAMESPACE "FCGALModule"
+
+IMPLEMENT_GAME_MODULE(FCGALModule, CGAL);
+
+FCGALModule* FCGALModule::m_Singleton(nullptr);
+
+FCGALModule& FCGALModule::Get()
+{
+	if (nullptr == m_Singleton) {
+		check(IsInGameThread());
+		FModuleManager::LoadModuleChecked<FCGALModule>("CGAL");
+	}
+	check(nullptr != m_Singleton);
+	return *m_Singleton;
+}
+
+void FCGALModule::StartupModule()
+{
+	m_Singleton = this;
+
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	FString CgalBaseDirRelative = IPluginManager::Get().FindPlugin("CGAL")->GetBaseDir();
+	FString BaseDir = FPaths::ConvertRelativePathToFull(CgalBaseDirRelative);
+
+	// Add on the relative location of the third party dll and load it
+	FString LIBGMP,LIBMPFR;
+#if PLATFORM_WINDOWS
+	LIBGMP = FPaths::Combine(*BaseDir, TEXT("ThirdParty/CGAL/libraries/Win64/libgmp-10.dll"));
+	LIBMPFR = FPaths::Combine(*BaseDir, TEXT("ThirdParty/CGAL/libraries/Win64/libmpfr-4.dll"));
+#endif // PLATFORM_WINDOWS
+
+	//UE_LOG(LogTemp, Log, TEXT("PluginsDir::FCGALModule::%s"), *LIBGMP);
+	//UE_LOG(LogTemp, Log, TEXT("PluginsDir::FCGALModule::%s"), *LIBMPFR);
+
+	LibgmpDllHandle = !LIBGMP.IsEmpty() ? FPlatformProcess::GetDllHandle(*LIBGMP) : nullptr;
+	libmpfrDllHandle = !LIBMPFR.IsEmpty() ? FPlatformProcess::GetDllHandle(*LIBMPFR) : nullptr;
+}
+
+void FCGALModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+
+	FPlatformProcess::FreeDllHandle(LibgmpDllHandle);
+	LibgmpDllHandle = nullptr;
+
+	FPlatformProcess::FreeDllHandle(libmpfrDllHandle);
+	libmpfrDllHandle = nullptr;
+}
+
+#undef LOCTEXT_NAMESPACE
+
